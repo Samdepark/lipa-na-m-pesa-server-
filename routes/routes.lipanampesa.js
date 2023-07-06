568af079f5ab0a6281052da76a03a8315487878f
samdepark <samdepark20@gmail.com>
2023-07-06 15:13:15 +0300

    solved all errors

--- routes/routes.lipanampesa.js
+++ routes/routes.lipanampesa.js
@@ -1,13 +1,11 @@
 const express = require ('express');
 const router = express.Router()
-const{
-    initiateSTKPush,
-    stkPushCallback,
-    confirmPayment
-} = require ('../controllers/controllers.lipanampesa');
+const initiateSTKPush = require ('../controllers/controllers.lipanampesa');
+const stkPushCallback = require ('../controllers/controllers.lipanampesa');
+const confirmPayment = require ('../controllers/controllers.lipanampesa');
 
 
-const {accessToken} = require ("../middlewares/middlewares.generateAccessToken.js");
+const accessToken = require ("../middlewares/middlewares.generateAccessToken.js");
 
 router.route('/stkPush').post(accessToken,initiateSTKPush)
 router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
